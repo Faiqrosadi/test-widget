0ab1a4c5dd6d3f74a597d39a8c5db177b6a1f101
faiqrosadi <faiqskripsi@gmail.com>
2024-03-01 13:16:03 +0700

    Initial Commit

---
+++ CryptoLive/CryptoLiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLiveApp.swift
+//  CryptoLive
+//
+//  Created by Macbook on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
